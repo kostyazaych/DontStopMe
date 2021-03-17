61764acf11de12e4459db03cd7b20446124c6d37
kostyazaych <kostya.from.rostov@gmail.com>
2021-03-17 20:41:46 +0300

    213

---
+++ DontStopMe/Source/DontStopMe/DontStopMeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DontStopMeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DONTSTOPME_API ADontStopMeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
